e9122dc6bedc610160c87472cc106abcd7ab6bde
eminavdovic <eminavdovic@gmail.com>
2022-11-23 11:52:10 +0100

    Ran into an unexpected syntax error in auth.js line 25. Vs Code doesnt show any error there. Will try to solve it.

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const DB =
   "mongodb+srv://emin:Muhalada_1990@cluster0.hfnwi67.mongodb.net/?retryWrites=true&w=majority";
 
 // middleware
+app.use(express.json());
 app.use(authRouter);
 
 // Connections
