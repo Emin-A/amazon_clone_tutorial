49d761816e0acfe45a2a7be926bd40898effe57c
eminavdovic <eminavdovic@gmail.com>
2022-11-23 19:48:20 +0100

    Error handling is constructed and ready to be used everywhere.

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -1,3 +1,7 @@
+import 'package:amazon_clone_tutorial/models/user.dart';
+import '../../../constants/global_variables.dart';
+import 'package:http/http.dart' as http;
+
 class AuthService {
   // sign up user
   void signUpUser({
@@ -5,6 +9,22 @@ class AuthService {
     required String password,
     required String name,
   }) async {
-    try {} catch (e) {}
+    try {
+      User user = User(
+        id: '',
+        name: name,
+        password: password,
+        email: email,
+        address: '',
+        type: '',
+        token: '',
+      );
+
+      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
+          body: user.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          });
+    } catch (e) {}
   }
 }
