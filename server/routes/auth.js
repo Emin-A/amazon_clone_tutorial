1a5e469c4b992c41d30d014c1bbf6b4bd689833a
eminavdovic <eminavdovic@gmail.com>
2022-11-24 11:47:05 +0100

    After hitting SignIn, created the same process like for SignUp, used jsonwebtoke to get a users data that only authenticated users can do that. Used bcrypt to compare and match the password in an if statement, received status 200 logged in with a specific token that we received!

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,6 +3,7 @@ const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 
 const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
 
 // SIGN UP
 authRouter.post("/api/signup", async (req, res) => {
@@ -26,6 +27,31 @@ authRouter.post("/api/signup", async (req, res) => {
     res.json(user);
   } catch (e) {
     res.status(500).json({ error: e.message });
+  }
+});
+
+// Sign In Route
+// Exercise
+authRouter.post("/api/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email does not exist!" });
+    }
+    //Muh...90
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Incorrect password." });
+    }
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ token, ...user._doc });
+  } catch (e) {
+    res.status(500).json;
+    ({ error: e.message });
   }
 });
 
