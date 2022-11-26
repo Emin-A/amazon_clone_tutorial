f513392248ec8e9c28189bd3b7f458b09fe784af
eminavdovic <eminavdovic@gmail.com>
2022-11-26 18:06:10 +0100

    The home screen UI is completed, in the app when we scroll down we get a sign - See all deals -. Next thing is the Admin Screen to do, since we want to add products and get a proper category.

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone_tutorial/features/home/widgets/address_box.dart';
+import 'package:amazon_clone_tutorial/features/home/widgets/carousel_image.dart';
+import 'package:amazon_clone_tutorial/features/home/widgets/deal_of_the_day.dart';
 import 'package:amazon_clone_tutorial/features/home/widgets/top_categories.dart';
 import 'package:flutter/material.dart';
 import '../../../constants/global_variables.dart';
@@ -83,14 +85,21 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: const [
-          AddressBox(),
-          SizedBox(
-            height: 10,
-          ),
-          TopCategories(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: const [
+            AddressBox(),
+            SizedBox(
+              height: 10,
+            ),
+            TopCategories(),
+            SizedBox(
+              height: 10,
+            ),
+            CarouselImage(),
+            DealOfTheDay(),
+          ],
+        ),
       ),
     );
   }
